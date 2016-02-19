9525cd32ce0c68a46263dd5cfd8cdbad9e7faeca
Jason Studstill <jason@jasonstudstill.com>
2016-02-18 23:49:30 -0800

    Pass departure city back to parent's state; Handle placeholder text on input

--- app/components/TravelOrigin.jsx
+++ app/components/TravelOrigin.jsx
@@ -3,13 +3,29 @@ import React from 'react';
 class TravelOrigin extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      setDepartureCity: (clickEvent) => {
+        this.props.actions.setDepartureCity(clickEvent.target.value);
+      }
+    }
   }
-
+  setPlaceholderText() {
+    // If the departure city has not already been defined, set the placeholder
+    // text to "Departure City"
+    return this.props.actions.getDepartureCity() ?
+      this.props.actions.getDepartureCity() :
+      "Departure City"
+  };
   render() {
     return (
       <div>
         <h1>Travel Origin</h1>
-        <button onClick={this.props.controllers.changeView.bind(this, 'TravelDestination')}>View TravelDestination</button>
+        <h2>Set Departure City and Travel Dates</h2>
+        <input placeholder={this.setPlaceholderText()} onChange={this.state.setDepartureCity.bind(this)}></input><br></br>
+        <button onClick={this.props.actions.setActiveComponent.bind(this, 'TravelDestination')}>
+          Show Random Destination
+        </button>
+
       </div>
     );
   }
