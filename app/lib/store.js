16f1bc666c3a40baf8c1f4754413d76c46219025
Jason Studstill <jason@jasonstudstill.com>
2016-02-21 01:24:59 -0800

    Add addition data fields to store

--- app/lib/store.js
+++ app/lib/store.js
@@ -1,10 +1,20 @@
+import destinationDataArray from '../lib/destinationData.js';
+
 const appDataStore = {
   activeComponent: 'TravelOrigin',
   departureCity: 'Departure City',
-  departureDate: '',
-  arrivalCity: '',
-  returnDate: '',
+  departureAirport: 'SEA',
+  departureDate: (new Date()).toISOString().split('T')[0], //Default to today
+  destinationCity: 'Destination City',
+  destinationAirport: 'SFO',
+  destinationData: destinationDataArray,
+  destinationImgUrl: '',
+  destinationImgAlt: '',
+  destinationInfo: '',
+  returnDate: (new Date()).toISOString().split('T')[0], //Default to today
   dateRangeObj: {},
+  flightRequest: {},
+  flightPrice: '',
 }
 
 export default appDataStore
