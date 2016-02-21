0e69cba5ea6250ef29246f3c49d825d55b15c3c9
Jason Studstill <jason@jasonstudstill.com>
2016-02-20 19:42:09 -0800

    Wire up all basic, necessary functionality

--- app/components/TravelOrigin.jsx
+++ app/components/TravelOrigin.jsx
@@ -1,31 +1,45 @@
 import React from 'react';
+import {DateRange} from 'react-date-range';
 
 class TravelOrigin extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      setDepartureCity: (clickEvent) => {
-        this.props.actions.setDepartureCity(clickEvent.target.value);
-      }
-    }
-  }
-  setPlaceholderText() {
-    // If the departure city has not already been defined, set the placeholder
-    // text to "Departure City"
-    return this.props.actions.getDepartureCity() ?
-      this.props.actions.getDepartureCity() :
-      "Departure City"
+      setDepartureCity: (changeEvent) => {
+        this.props.actions.setDepartureCity(changeEvent.target.value);
+      },
+      departureCity: this.props.actions.getDepartureCity(),
+      setTravelDates: (dateRangeObj) => {
+        this.props.actions.setDepartureDate(dateRangeObj.startDate._d.toISOString().slice(0,10));
+        this.props.actions.setReturnDate(dateRangeObj.endDate._d.toISOString().slice(0,10));
+        if (dateRangeObj.startDate._d != dateRangeObj.endDate._d)
+          this.props.actions.setDateRangeObj(dateRangeObj);
+      },
+      destinationCity: this.props.actions.getDestinationCity(),
+    };
   };
+  handleFormSubmit(event) {
+    event.preventDefault();
+    this.props.actions.setActiveComponent('TravelDestination');
+  }
   render() {
     return (
       <div>
         <h1>Travel Origin</h1>
         <h2>Set Departure City and Travel Dates</h2>
-        <input placeholder={this.setPlaceholderText()} onChange={this.state.setDepartureCity.bind(this)}></input><br></br>
-        <button onClick={this.props.actions.setActiveComponent.bind(this, 'TravelDestination')}>
-          Show Random Destination
-        </button>
-
+        <form onSubmit={this.handleFormSubmit.bind(this)}>
+          <label htmlFor="departureCityInput">Flying from: </label>
+          <input id='departureCityInput' placeholder={this.state.departureCity} onChange={this.state.setDepartureCity.bind(this)}></input><br></br>
+          <input type="submit" value="Show Random Destination" />
+          <DateRange
+            calendars="1"
+            onChange={this.state.setTravelDates}
+            width="100%"
+            />
+        </form>
+        <h3>Departing from: {this.props.actions.getDepartureCity()}</h3>
+        <h3>Departing on: {this.props.actions.getDepartureDate()}</h3>
+        <h3>Returning on: {this.props.actions.getReturnDate()}</h3>
       </div>
     );
   }
