a97704a6f7a2b31ce3e2eb88c46a5bd0e33c5da5
Jason Studstill <jason@jasonstudstill.com>
2016-02-20 19:41:16 -0800

    Factor out DATA_STORE and ACTIONS to modular files

--- app/app.jsx
+++ app/app.jsx
@@ -2,49 +2,25 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TravelDestination from './components/TravelDestination.jsx';
-import TravelOrigin from './components/TravelOrigin.jsx';
+import ACTIONS from './lib/actions.js';
+import DATA_STORE from './lib/store.js';
+import ROUTER from './lib/router.js';
 
 class Main extends React.Component {
   constructor(props){
     super(props);
     // Maintain state (data store) for the entire app; Simple alternative to Redux
-    this.state = {
-      activeComponent: 'TravelOrigin',
-      departureCity: '',
-    };
+    this.state = DATA_STORE;
   }
 
   render() {
-    // Actions - This actions object contains all of the functionality for
-    // getting and retrieving state data for the app
-    const actions = {
-      setActiveComponent: (componentName) => {
-        this.setState({activeComponent: componentName});
-      },
-      getActiveComponent: () => {
-        return this.state.activeComponent;
-      },
-      setDepartureCity: (dc) => {
-        this.setState({departureCity: dc});
-      },
-      getDepartureCity: () => {
-        return this.state.departureCity;
-      }
-    }
+    // ACTIONS - contains functionality for getting and retrieving state data
+    const actions = ACTIONS(this);
 
     // Handle Routing
-    const router = {view: ''};
-    switch (this.state.activeComponent) {
-      case 'TravelOrigin':
-      router.view = <TravelOrigin actions={actions} />;
-      break;
-      case 'TravelDestination':
-      router.view = <TravelDestination actions={actions} />;
-      break;
-      default:
-      router.view = <TravelOrigin actions={actions} />;
-    }
+    const route = {};
+    // Instantiate ROUTER with actions and route object
+    ROUTER(this, actions, route);
 
     // Render the App
     return (
@@ -54,7 +30,7 @@ class Main extends React.Component {
            we show a "start over" button.  */}
         {this.state.activeComponent !== 'TravelOrigin' ?
           (<button onClick={actions.setActiveComponent.bind(this, 'TravelOrigin')}>Start Over</button>) : null}
-        {router.view}
+        {route.view}
       </div>
     )
   }
