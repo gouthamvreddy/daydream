46323540d8d42fe7a53665625d104506af8377e8
Jason Studstill <jason@jasonstudstill.com>
2016-02-27 17:37:36 -0800

    Pass appState down to child components through appState prop

--- app/lib/router.js
+++ app/lib/router.js
@@ -3,16 +3,16 @@ import TravelDestination from '../components/TravelDestination.jsx';
 import TravelOrigin from '../components/TravelOrigin.jsx';
 
 // Handle Routing
-let handleRoutes = (MainComponent, actions, route) => {
+let handleRoutes = (MainComponent, actions, route, state) => {
   switch (MainComponent.state.activeComponent) {
     case 'TravelOrigin':
-    route.view = <TravelOrigin actions={actions} />;
+    route.view = <TravelOrigin actions={actions} appState={state}/>;
     break;
     case 'TravelDestination':
-    route.view = <TravelDestination actions={actions} />;
+    route.view = <TravelDestination actions={actions} appState={state}/>;
     break;
     default:
-    route.view = <TravelOrigin actions={actions} />;
+    route.view = <TravelOrigin actions={actions} appState={state}/>;
   }
 }
 
