579f0da84e0bc55f9621e11dd50560671026365c
Jason Studstill <jason@jasonstudstill.com>
2016-02-27 17:36:25 -0800

    Nail down all async activity; handle edge cases

--- app/components/TravelDestination.jsx
+++ app/components/TravelDestination.jsx
@@ -1,62 +1,85 @@
 import React from 'react';
 import request from 'superagent';
 import destinationDataArray from '../lib/destinationData.js';
+import ThingsToDo from './ThingsToDo.jsx'
 
 class TravelDestination extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      setDestinationInfo: this.props.actions.setDestinationInfo
+      destinationCity: '',
+      flightPrice: '',
+      appState: this.props.appState
     };
   }
 
   clarifySearchQuery() {
     return new Promise((resolve, reject) => {
-      // Default to Seattle if no input is given
-      let cityQuery = (this.props.actions.getDepartureCity() != 'Departure City' ?
-      this.props.actions.getDepartureCity() :
-      'Seattle');
-      request.get(`/api/suggestions_and_resolutions/${cityQuery}`)
-      .end((err, res) => {
-        // Reject the promise if error
-        if (err) {
-          this.executeCircleOfLife()
-        } else {
-          let departureCity = JSON.parse(res.text).f.split(',').splice(0,2).join(", ");
-          let departureAirport = JSON.parse(res.text).a;
-          console.log('airpoort ' + departureAirport)
-          this.props.actions.setDepartureCity(departureCity);
-          this.props.actions.setDepartureAirport(departureAirport);
-          // Resolve the promise
-          let data = {
-            departureCity: departureCity,
-            departureAirport: departureAirport
+      let departureCity = this.props.actions.getDepartureCity();
+      let departureAirport = this.props.actions.getDepartureAirport();
+      let data = {
+        departureCity: departureCity,
+        departureAirport: departureAirport
+      }
+      // If you have already clarified the search input, skip the ajax
+      if (this.props.appState.departureCityIsClarified) {
+        resolve(data);
+      } else {
+        // Default to Seattle if no input is given
+        let cityQuery = (this.props.actions.getDepartureCity() === 'Departure City' ?
+        'Seattle' : departureCity);
+        request.get(`/api/suggestions_and_resolutions/${cityQuery}`)
+        .end((err, res) => {
+          // Reject the promise if error
+          if (err) {
+            this.executeCircleOfLife()
+          } else {
+            departureCity = JSON.parse(res.text).f.split(',').splice(0,2).join(", ");
+            departureAirport = JSON.parse(res.text).a;
+            console.log('Departure Airport: ' + departureAirport)
+            console.log('Departure City: ' + departureCity)
+            this.props.actions.setDepartureCity(departureCity);
+            this.props.actions.setDepartureAirport(departureAirport);
+            this.props.actions.departureCityIsClarified(true);
+            // Resolve the promise
+            data = {
+              departureCity: departureCity,
+              departureAirport: departureAirport
+            }
+            resolve(data);
           }
-          resolve(data);
-        }
-      });
-    })
+        })
+      }
+    });
   }
 
   pickRandomDestination(data) {
     return new Promise((resolve, reject) => {
-      let destinationObj = destinationDataArray[
-        Math.floor(Math.random() * destinationDataArray.length - 1)
-      ];
-      this.state.setDestinationInfo(destinationObj);
-      if (destinationObj['airportCode']) {
-        data.arrivalAirport = destinationObj['airportCode'];
-        console.log('departure airport: ' + data.departureAirport);
-        resolve(data);
-      } else {
-        reject()
+      // Pick random destination from pre-selected options hard-coded
+      // in lib/destinationData.js
+      let getRandomDest = () => {
+        return destinationDataArray[
+          Math.floor(Math.random() * destinationDataArray.length - 1)
+        ];
       }
+      let destinationObj = getRandomDest();
+      // Ensure that randomly chosen destination from the destinations
+      // array has an airportCode - it should
+      while (!destinationObj['airportCode']) {
+        destinationObj = getRandomDest();
+      }
+      this.props.actions.setDestinationInfo(destinationObj);
+      data.arrivalAirport = destinationObj['airportCode'];
+      data.destinationCity = destinationObj['cityName'];
+      this.setState({destinationCity: destinationObj['cityName']})
+      resolve(data);
     })
   }
 
   fetchFlightInfo(data) {
-    console.log('random destination airport: ' + data.arrivalAirport);
-    let flightPromise = new Promise((resolve, reject) => {
+    console.log(`Destination City: ${data.destinationCity}`);
+    console.log(`Destination airport: ${data.arrivalAirport}`);
+    return new Promise((resolve, reject) => {
       if (this.state.flightRequest) this.state.flightRequest.abort();
       let flightRequest = request.get(`/api/flight_search/`)
       .query({departureAirport: data.departureAirport})
@@ -64,40 +87,41 @@ class TravelDestination extends React.Component {
       .query({departureDate: this.props.actions.getDepartureDate()})
       .query({returnDate: this.props.actions.getReturnDate()})
       .end((err, res) => {
-        if (!err) resolve(res);
+        if (!err) {
+          data.res = res;
+          resolve(data);
+        }
         // else reject(alertError(err));
       });
       this.setState({flightRequest: flightRequest});
-    });
+    })
     flightPromise.then((res) => {
       this.updateOnResponse(res);
     })
   }
 
-  updateOnResponse(res) {
-    let resObj = JSON.parse(res.text);
+  updateOnResponse(data) {
+    let resObj = JSON.parse(data.res.text);
       // If there is no available flight given the input, the compenent
       // is re-rendered to search for new options
       if (!resObj.offers || !resObj.offers[0] || !resObj.offers[0].totalFare) {
         this.executeCircleOfLife();
       } else {
         let cheapestFlight = resObj.offers[0];
-        console.log(cheapestFlight);
+        // Add more functionality
+        this.props.actions.setFlightPrice(cheapestFlight.totalFare);
         this.setState({
-          price: cheapestFlight.totalFare,
-          detailsUrl: cheapestFlight.detailsUrl
+          flightPrice: cheapestFlight.totalFare,
+          detailsUrl: cheapestFlight.detailsUrl.toString()
         });
       }
   }
 
   executeCircleOfLife() {
-    // this.clarifySearchQuery().then(
-    //   this.pickRandomDestination().then(
-    //     this.fetchFlightInfo()
-    //   )
-    // )
-    this.clarifySearchQuery().then(this.pickRandomDestination.bind(this)).then(this.fetchFlightInfo.bind(this));
-    // this.pickRandomDestination().then(this.clarifySearchQuery).then(this.fetchFlightInfo)
+    this.clarifySearchQuery()
+      .then(this.pickRandomDestination.bind(this))
+      .then(this.fetchFlightInfo.bind(this))
+      .then(this.updateOnResponse.bind(this));
   }
 
   alertError(err) {
@@ -110,19 +134,35 @@ class TravelDestination extends React.Component {
 
   handleFormSubmit(event) {
     event.preventDefault();
+    this.setState({flightPrice: ''});
     this.executeCircleOfLife();
   };
 
   render() {
+    let flightPrice = `\$${this.props.actions.getFlightPrice()}`;
     return (
       <div>
-        <h1>Travel Destination</h1>
         <h2>Departure City: {this.props.actions.getDepartureCity()}</h2>
-        <h2>Random Destination: {this.props.actions.getDestinationCity()}</h2>
         <form onSubmit={this.handleFormSubmit.bind(this)}>
-          <label htmlFor="pickRandomDestination">Click for a new destination</label>
-          <input type="submit" value="New Destination" />
+          <label htmlFor="pickRandomDestination"></label>
+          <input type="submit" value="New Random Destination" />
         </form>
+        {this.props.actions.getDestinationCity() === 'Destination City' ?
+          <h2>Getting Random Destination...</h2> :
+          <h2>Random Destination: {this.props.actions.getDestinationCity()}</h2>
+        }
+        {this.state.flightPrice ?
+          <div>
+            <h3><a href={this.state.detailsUrl} target="_blank">Book Now for {flightPrice}</a></h3>
+          </div> :
+          <h3>Getting the cheapest flight...</h3>
+        }
+        <ThingsToDo
+          actions={this.props.actions}
+          appState={this.state.appState}
+          destinationCity={this.state.destinationCity}
+        />
+
       </div>
     );
   }
